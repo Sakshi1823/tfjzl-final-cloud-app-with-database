01857332dfad5e1f271cde432acab6325c01d585
Sakshi1823 <sakshihingamire18@gmail.com>
2026-04-26 08:15:59 -0400

    Completed task 3 course detail template

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -103,7 +103,6 @@ class Question(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     question_text = models.CharField(max_length=500)
     grade = models.IntegerField()
-
     def __str__(self):
         return self.question_text
 
@@ -115,18 +114,13 @@ class Question(models.Model):
         ).count()
 
         return all_answers == selected_correct
-
-
 # Choice model
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
-
     def __str__(self):
         return self.choice_text
-
-
 # Submission model
 class Submission(models.Model):
     enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
